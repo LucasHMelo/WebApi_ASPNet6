1069104d636f39fe5ceb242ebca07cf350657b6d
Lucas Melo <Lucas Melo@DESKTOP-VAAMSO0>
2022-03-30 23:33:19 -0300

    Adicionar arquivos de projeto.

---
+++ webCrudnet6/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webCrudnet6.Models;
+
+namespace webCrudnet6.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private static List<User> Users()
+        {
+            return new List<User> { new User { Nome = "Lucas", Id = 123456, DataNascimento = new DateTime(2000,01,01)}
+            };
+        }
+
+        [HttpGet]
+        public IActionResult Get() { return Ok(Users()); }
+
+        [HttpPost]
+        public IActionResult Post(User user) { 
+            
+            var users = Users();
+            users.Add(user);
+
+            return Ok(users); 
+        }
+    }
+}
---
+++ webCrudnet6/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace webCrudnet6.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+    }
+}
